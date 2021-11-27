e2037eaf06bde74ba12f7a756ca3a55a45e7716a
yixmlh90 <70496788+yixmlh90@users.noreply.github.com>
2021-11-27 22:46:29 +0800

    Create BJ.js

---
+++ BJ.js
@@ -0,0 +1,15 @@
+hostname = api.m.jd.com, trade-acs.m.taobao.com, 
+
+#京东历史比价-通知
+# yichahucha
+#^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price_lite.js
+
+# Tartarus2014
+^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/jd_price_lite.js
+
+#京东历史比价-内置版 (by yichahucha)
+;^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+
+#淘宝历史比价-通知版(by yichahucha)
+;^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price_lite.js
+;^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price_lite.js
