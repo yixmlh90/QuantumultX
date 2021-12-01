c2fd804af058a0ed07cd7b23f7f5105659aaaac3
yixmlh90 <70496788+yixmlh90@users.noreply.github.com>
2021-12-01 23:02:04 +0800

    Update BJ.js

--- BJ.js
+++ BJ.js
@@ -2,10 +2,10 @@ hostname = api.m.jd.com, trade-acs.m.taobao.com,
 
 #京东历史比价-通知
 # yichahucha
-#^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price_lite.js
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price_lite.js
 
 # Tartarus2014
-^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/jd_price_lite.js
+;^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/jd_price_lite.js
 
 #京东历史比价-内置版 (by yichahucha)
 ;^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
